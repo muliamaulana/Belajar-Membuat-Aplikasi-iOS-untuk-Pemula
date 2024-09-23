bd731a0a1934f098a18b421b3e5da91692650054
muliamaulana <m.muliamaulana@gmail.com>
2024-09-23 08:11:45 +0700

    Initial Commit

---
+++ Resep Masak/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Resep Masak
+//
+//  Created by muliamaulana on 23/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
